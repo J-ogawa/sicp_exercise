ad6da124a79ab6cdf466909cdfcc412d0e4f288d
J-ogawa <conilus@gmail.com>
2015-09-28 20:09:48 +0900

    2.5

--- 2/2.5.scm
+++ 2/2.5.scm
@@ -7,5 +7,28 @@
         sum)))
   (itelate 1 a b))
 
+(define (car c)
+  (define (itelate c a)
+  (if (even? c)
+    (itelate (/ c 2) (+ a 1))
+    a))
+  (itelate c 0))
+
+(define (cdr c)
+  (define (itelate c b)
+    (if (even? c)
+      (itelate (/ c 2) b)
+      (if (> c 1)
+        (itelate (/ c 3) (+ b 1))
+        b)))
+  (itelate c 0))
+
+
 (define (test1)
   (cons 2 5))
+
+(define (test2)
+  (car (cons 2 5)))
+
+(define (test3)
+  (cdr (cons 2 5)))
