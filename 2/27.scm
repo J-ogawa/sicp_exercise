e0f8edd3d82163d589ee428b54222c42f1dd6ea1
J-ogawa <conilus@gmail.com>
2015-10-29 20:05:29 +0900

    2.27

---
+++ 2/27.scm
@@ -0,0 +1,16 @@
+(define (reverse- l)
+  (define (iter src dst)
+    (if (null? src)
+      dst
+      (iter (cdr src) (cons (car src) dst))))
+  (iter l (list)))
+
+(define (deep-reverse l)
+  (define (iter src dst)
+    (if (null? src)
+      dst
+      (if (pair? (car src))
+        (iter (cdr src) (cons (iter (car src) (list)) dst))
+        (iter (cdr src) (cons (car src) dst)))))
+  (iter l (list)))
+
