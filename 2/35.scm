213938692d48c83609d55c5b9ea330033c057c07
J-ogawa <conilus@gmail.com>
2015-11-19 20:07:44 +0900

    2.35

--- 2/35.scm
+++ 2/35.scm
@@ -5,12 +5,10 @@
         (accumulate op initial (cdr sequence)))))
 
 (define (count-leaves t)
-  (accumulate + 0 (map (lambda (x) (cond
-                                     ((null? x) 0)
-                                     ((not (pair? x)) 1)
-                                     ((not (pair? (car x))) (+ (count-leaves (list (car x))) (count-leaves (cdr x))))
-                                     (else (+ (count-leaves (car x)) (count-leaves (cdr x))))))
+  (accumulate + 0 (map (lambda (x) (if (pair? x)
+                                     (count-leaves x)
+                                     1))
                        t)))
 
 (define (test)
-  (count-leaves (list (list 1 2) (list 3 4 (list 5 6 7 8 9 (list 10))))))
+  (count-leaves (list 0 (list 1 2) (list 3 4 (list 5 6 7 8 9 (list 10))))))
