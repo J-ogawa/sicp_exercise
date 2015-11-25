4de926fb9b7fb54360dfbac1ea954ea8f304710d
J-ogawa <conilus@gmail.com>
2015-11-25 20:07:08 +0900

    2.37

--- 2/37.scm
+++ 2/37.scm
@@ -10,8 +10,12 @@
     (cons (accumulate op init (map (lambda(seq)(car seq)) seqs))
           (accumulate-n op init (map (lambda(seq)(cdr seq)) seqs)))))
 
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
 (define (matrix-*-vector m v)
-  (map (lambda(v-of-m)(map * v v-of-m)) m))
+  (map (lambda(v-of-m)(dot-product v v-of-m)) m))
+ ; (map (lambda(v-of-m)(map * v v-of-m)) m))
 
 (define (transpose mat)
   (accumulate-n cons (list) mat))
@@ -19,7 +23,7 @@
 
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
-    (map (accumulate-n + 0 (map * cols m)))
+    (map (lambda(mv)(matrix-*-vector cols mv)) m)))
 
 (define (test-matrix-*-vector)
   (matrix-*-vector (list (list 1  2  3  4)
@@ -31,6 +35,7 @@
   (transpose (list (list 1  2  3  4)
                          (list 5  6  7  8)
                          (list 9 10 11 12))))
+
 (define (test-matrix-*-matrix)
   (matrix-*-matrix (list (list 1  2  3  4)
                          (list 5  6  7  8)
@@ -39,3 +44,10 @@
                          (list  5  6  7)
                          (list  8  9 10)
                          (list 11 12 13))))
+
+(define (test-matrix-*-matrix2)
+  (matrix-*-matrix (list (list 1 2 3)
+                         (list 4 5 6))
+                   (list (list 7 10)
+                         (list 8 11)
+                         (list 9 12))))
