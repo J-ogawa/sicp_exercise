5015865869a11b3d8839b59b9f910aebcfab0d20
J-ogawa <conilus@gmail.com>
2016-02-04 20:21:45 +0900

    2.52

--- 2/52.scm
+++ 2/52.scm
@@ -12,3 +12,15 @@
             (corner (corner-split painter (- n 1))))
         (beside (below painter (beside top-left top-left))
                 (below bottom-right corner))))))
+
+; c
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (be painter))))
+      (below bottom top))))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  (if (= n 0) (flip-horiz rotate) rotate180) flip-vert)))
+    (combine4 (corner-split painter n))))
